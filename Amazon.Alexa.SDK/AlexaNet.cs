bff40d57176d2271201b9aa77e850817a387d54f
Bob Lautenbach <boblautenbach@braneworks.com>
2017-03-06 19:11:58 -0500

    Added support for attributed classes OR passing in a list of objects

--- Amazon.Alexa.SDK/AlexaNet.cs
+++ Amazon.Alexa.SDK/AlexaNet.cs
@@ -11,7 +11,16 @@ namespace Amazon.Alexa.SDK
 {
     public sealed class AlexaNet: IAlexaNetSDK
     {
-        private static readonly ConcurrentDictionary<string, string> HandlerDict = new ConcurrentDictionary<string, string>();
+        private static readonly ConcurrentDictionary<string, string> _handlerDict = new ConcurrentDictionary<string, string>();
+        private static ConcurrentBag<object> _handlerList = new ConcurrentBag<object>();
+        private static Mode _mode;
+        private enum Mode
+        {
+            Attributed,
+            Func,
+            HandlerList
+
+        }
 
         private static readonly Lazy<AlexaNet> sdk =
             new Lazy<AlexaNet>(() => new AlexaNet());
@@ -24,9 +33,34 @@ namespace Amazon.Alexa.SDK
 
         public static void RegisterHandlers()
         {
-            if (!HandlerDict.Any())
+            _mode = Mode.Attributed;
+            if (!_handlerDict.Any())
             {
                 LoadSDKAttributedClasses();
+            }
+        }
+
+        public static void RegisterHandlers(List<object> handlers)
+        {
+            _mode = Mode.HandlerList;
+            if (!_handlerDict.Any())
+            {
+                _handlerList = new ConcurrentBag<object>(handlers);
+                LoadClasses(handlers);
+            }
+        }
+
+        private static void LoadClasses(List<object> handlers)
+        {
+            foreach (var c in handlers)
+            {
+                var intentMgr = c.GetType();
+                MethodInfo[] methodInfos = Type.GetType(intentMgr.AssemblyQualifiedName)
+                           .GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+                foreach (var m in methodInfos)
+                {
+                    _handlerDict.TryAdd(m.Name, intentMgr.AssemblyQualifiedName);
+                }
             }
         }
 
@@ -39,7 +73,7 @@ namespace Amazon.Alexa.SDK
                            .GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
                 foreach(var m in methodInfos)
                 {
-                    HandlerDict.TryAdd(m.Name, c.AssemblyQualifiedName);
+                    _handlerDict.TryAdd(m.Name, c.AssemblyQualifiedName);
                 }
             }
         }
@@ -53,7 +87,7 @@ namespace Amazon.Alexa.SDK
                 var func = (string)(string.IsNullOrEmpty(request.Request.Intent.Name) ? request.Request.Type : request.Request.Intent.Name);
                 func = func.Replace("Amazon.", "").Replace("AMAZON.", "");
 
-                var target = HandlerDict.FirstOrDefault(x => x.Key == func);
+                var target = _handlerDict.FirstOrDefault(x => x.Key == func);
 
                 var intentMgr = Type.GetType(target.Value);
 
@@ -64,26 +98,44 @@ namespace Amazon.Alexa.SDK
                     throw new Exception("Not found");
                 }
 
-                //Handles static classes or methods as well as instance classes
-                if (intentMgr.IsAbstract || method.IsAbstract)
+
+                switch (_mode)
                 {
-                    //don't instantiate, its a static class and/or method
-                    intentResponse = method.Invoke(intentMgr, new object[] { request, response });
-                }else
-                {
-                    //Need to conside that this is really creating a new object for every call vs
-                    //getting a refernce to an object and calling methods.  This also requires
-                    //a parameterless class and the main issue is related to using you API controller as 
-                    //your intent handler ("this").  In that instance, you would be creating an 
-                    //instance of your API controller for all intent requests (even though the API object
-                    //already exists
-                    //How do we allow for singletons?
-                    //How about constructors with params?
-                    //What if we store reference to object after first instantiation?
-                    ConstructorInfo typeContructor = intentMgr.GetConstructor(Type.EmptyTypes);
-                    typeInstance = typeContructor.Invoke(new object[] { });
-                    intentResponse = method.Invoke(typeInstance, new object[] { request, response });
+                    case Mode.Attributed:
+                        //  handlers.FirstOrDefault(x => x.GetType().AssemblyQualifiedName == "")
+                        //Handles static classes or methods as well as instance classes
+                        if (intentMgr.IsAbstract || method.IsAbstract)
+                        {
+                            //don't instantiate, its a static class and/or method
+                            intentResponse = method.Invoke(intentMgr, new object[] { request, response });
+                        }
+                        else
+                        {
+                            //Need to conside that this is really creating a new object for every call vs
+                            //getting a refernce to an object and calling methods.  This also requires
+                            //a parameterless class and the main issue is related to using you API controller as 
+                            //your intent handler ("this").  In that instance, you would be creating an 
+                            //instance of your API controller for all intent requests (even though the API object
+                            //already exists
+                            //How do we allow for singletons?
+                            //How about constructors with params?
+                            //What if we store reference to object after first instantiation?
+                            ConstructorInfo typeContructor = intentMgr.GetConstructor(Type.EmptyTypes);
+                            typeInstance = typeContructor.Invoke(new object[] { });
+                            intentResponse = method.Invoke(typeInstance, new object[] { request, response });
+                        }
+                        break;
+                    case Mode.Func:
+                        break;
+                    case Mode.HandlerList:
+                        //this is for handling passing a list of objects (vs using attributed classes
+                        typeInstance = _handlerList.FirstOrDefault(x => x.GetType().AssemblyQualifiedName == target.Value);
+                        intentResponse = method.Invoke(typeInstance, new object[] { request, response });
+                        break;
+                    default:
+                        break;
                 }
+
 
                 //set type to null to prompt garbage collection
                 //perhaps require intent handlers to implemention IDisposable??
@@ -97,7 +149,7 @@ namespace Amazon.Alexa.SDK
             }
         }
 
-        public static bool ShouldIgnoreClass(Type t)
+        private static bool ShouldIgnoreClass(Type t)
         {
             // Get instance of the attribute.
             AlexaNetSDK attrib =
